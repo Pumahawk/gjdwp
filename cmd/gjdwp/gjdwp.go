f28002e48161572c286ebeff22e0f5bfef0bf406
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-04-12 16:07:55 +0200

    restart

--- cmd/gjdwp/gjdwp.go
+++ cmd/gjdwp/gjdwp.go
@@ -2,45 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/Pumahawk/gjdwp/lib/jdwp"
 )
 
 func main() {
-	c, err := jdwp.Dial("localhost:5005")
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-	pack, _ := c.Next()
-	switch pack := pack.(type) {
-	case *jdwp.ReplyPack:
-		log.Printf("reply pack=%v\n", pack)
-	case *jdwp.CommandPack:
-		switch event := pack.Data.(type) {
-		case *jdwp.Event:
-			for i, eventData := range event.EventData {
-				i := i + 1
-				switch eventData.(type) {
-				case *jdwp.VMStartEventData:
-					log.Printf("command pack: event: (%d/%d) vmstartevent SuspendPolicy=%q\n", i, event.Events, event.SuspendPolicy)
-				default:
-					log.Printf("command pack: event: (%d/%d) unknown data type %T\n", i, event.Events, event.EventData)
-				}
-			}
-		default:
-			log.Printf("command pack: unknown data type %T\n", pack.Data)
-		}
-	default:
-		panic(fmt.Errorf("main unknown pack type %T", pack))
-	}
-
-	log.Printf("Write ResumeCommandData")
-	if err := c.SandVirtualMachineResume(); err != nil {
-		log.Printf("Error on write pack: %s", err)
-	} else {
-		log.Printf("Success write pack")
-	}
-	ch := make(chan int)
-	<-ch
+	fmt.Println("Hello world!")
 }
